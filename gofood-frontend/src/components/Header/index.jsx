9dcf18cc51c7b1055fc62de37dfa6cb15e64ad27
LEHOANGGLAM <lelam1384@gmail.com>
2023-03-31 00:09:31 +0700

    add header and footer

--- gofood-frontend/src/components/Header/index.jsx
+++ gofood-frontend/src/components/Header/index.jsx
@@ -1,82 +1,149 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-
-import styles from './Header.module.scss';
-
+import logo from '../../assets/img/logo.png';
+import language from '../../assets/img/language.png';
+import '../../assets/sass/_header.scss';
 
 const Header = () => {
-
-
   return (
-    <header className={styles.header}>
-      <div className={styles.header__top}>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 col-md-5">
-              <div className={styles.header__top__left}>
-                <ul>
-                  <li><i className="fa fa-envelope" />admin@mfe.com</li>
-                  <li>Free shipping for all orders over $50</li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-6 col-md-7">
-              <div className={styles.header__top__right}>
-                <div className={styles.header__top__right__social}>
-                  <a href="#"><i className="fa fa-facebook" /></a>
-                  <a href="#"><i className="fa fa-twitter" /></a>
-                  <a href="#"><i className="fa fa-linkedin" /></a>
-                  <a href="#"><i className="fa fa-pinterest-p" /></a>
-                </div>
-                <div className={styles.header__top__right__language}>
-                  <img src="/img/language.png" alt="" />
-                  <div>English</div>
-                  <span className="arrow_carrot-down" />
-                  <ul>
-                    <li><a href="#">Spanish</a></li>
-                    <li><a href="#">English</a></li>
-                  </ul>
-                </div>
-                {/* <SignIn /> */}
-              </div>
-            </div>
+    <div>
+      
+      <div class="humberger__menu__overlay"></div>
+      <div class="humberger__menu__wrapper">
+        <div class="humberger__menu__logo">
+          <a href="#"><img src={logo} alt="logo" /></a>
+        </div>
+        <div class="humberger__menu__cart">
+          <ul>
+            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
+            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+          </ul>
+          <div class="header__cart__price">item: <span>$150.00</span></div>
+        </div>
+        <div class="humberger__menu__widget">
+          <div class="header__top__right__language">
+            <img src={language} alt="lang" />
+            <div>English</div>
+            <span class="arrow_carrot-down"></span>
+            <ul>
+              <li><a href="#">Spanis</a></li>
+              <li><a href="#">English</a></li>
+            </ul>
           </div>
+          <div class="header__top__right__auth">
+            <a href="#"><i class="fa fa-user"></i> Login</a>
+          </div>
+        </div>
+        <nav class="humberger__menu__nav mobile-menu">
+          <ul>
+            <li class="active"><a href="./index.html">Home</a></li>
+            <li><a href="./shop-grid.html">Shop</a></li>
+            <li><a href="#">Pages</a>
+              <ul class="header__menu__dropdown">
+                <li><a href="./shop-details.html">Shop Details</a></li>
+                <li><a href="./shoping-cart.html">Shoping Cart</a></li>
+                <li><a href="./checkout.html">Check Out</a></li>
+                <li><a href="./blog-details.html">Blog Details</a></li>
+              </ul>
+            </li>
+            <li><a href="./blog.html">Blog</a></li>
+            <li><a href="./contact.html">Contact</a></li>
+          </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+        <div class="header__top__right__social">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-pinterest-p"></i></a>
+        </div>
+        <div class="humberger__menu__contact">
+          <ul>
+            <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
+            <li>Free Shipping for all Order of $99</li>
+          </ul>
         </div>
       </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3">
-            <div className={styles.header__logo}>
-              <Link to="/">
-                <img src="/img/logo.png" alt="" />
-              </Link>
+      {/* <!-- Header Section Begin --> */}
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="header__top__left">
+                            <ul>
+                                <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
+                                <li>Free Shipping for all Order of $99</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="header__top__right">
+                            <div class="header__top__right__social">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                            </div>
+                            <div class="header__top__right__language">
+                                <img src={language} alt="lang"/>
+                                <div>English</div>
+                                <span class="arrow_carrot-down"></span>
+                                <ul>
+                                    <li><a href="#">Spanis</a></li>
+                                    <li><a href="#">English</a></li>
+                                </ul>
+                            </div>
+                            <div class="header__top__right__auth">
+                                <a href="#"><i class="fa fa-user"></i> Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="col-lg-6">
-            {/* <Nav /> */}
-          </div>
-          <div className="col-lg-3">
-            <div className={styles.header__cart}>
-              <ul>
-                <li>
-                  <a href="#"><i className="fa fa-heart" /> <span>1</span></a>
-                </li>
-                <li>
-                 
-                </li>
-              </ul>
-              <div className={styles.header__cart__price}>
-               
-              </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="header__logo">
+                        <a href="./index.html"><img src={logo} alt="logo"/></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <nav class="header__menu">
+                        <ul>
+                            <li class="active"><a href="./index.html">Home</a></li>
+                            <li><a href="./shop-grid.html">Shop</a></li>
+                            <li><a href="#">Pages</a>
+                                <ul class="header__menu__dropdown">
+                                    <li><a href="./shop-details.html">Shop Details</a></li>
+                                    <li><a href="./shoping-cart.html">Shoping Cart</a></li>
+                                    <li><a href="./checkout.html">Check Out</a></li>
+                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./blog.html">Blog</a></li>
+                            <li><a href="./contact.html">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-3">
+                    <div class="header__cart">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
+                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+                        </ul>
+                        <div class="header__cart__price">item: <span>$150.00</span></div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="humberger__open">
+                <i class="fa fa-bars"></i>
+            </div>
         </div>
-        <div className={styles.hamburger__open}>
-          <i className="fa fa-bars" />
-        </div>
-      </div>
     </header>
+    {/* <!-- Header Section End --> */}
+    </div>
   );
 };
 
