706fe68d03249c9a6d305e5f37182476797d93d7
QuocHung <tqhung1234@gmail.com>
2023-04-30 18:45:45 +0700

    add order api and approve

--- djangoproject/djangoapp/urls.py
+++ djangoproject/djangoapp/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include
 from . import views
+from .views import StoreActiveView
 
 router = routers.DefaultRouter()
 router.register('categories', views.CategoryViewSet)
@@ -8,7 +9,10 @@ router.register('foods', views.FoodViewSet)
 router.register('stores', views.StoreViewSet)
 router.register('menus', views.MenuViewSet)
 router.register('users', views.UserViewSet)
+router.register('order', views.OrderViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('stores/<int:id>/active/', StoreActiveView.as_view(), name='store-active'),
 ]
